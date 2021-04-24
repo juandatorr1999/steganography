67168c70d2bc279310242885271753c9cbba0d47
Juan David Torres <juandatorr_1999@hotmail.com>
2021-04-23 19:03:47 -0500

    Solved, shows the image now. Only works capacitor

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import {ImagePicker} from '@ionic-native/image-picker/ngx'
+
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -7,6 +9,45 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  imageResponse: any;
+  options: any;
+
+  constructor(private imagePicker: ImagePicker) { }
+
+  
+  public getImage(){
+    this.options = {
+      // Android only. Max images to be selected, defaults to 15. If this is set to 1, upon
+      // selection of a single image, the plugin will return it.
+      maximumImagesCount: 1,
+
+      // max width and height to allow the images to be.  Will keep aspect
+      // ratio no matter what.  So if both are 800, the returned image
+      // will be at most 800 pixels wide and 800 pixels tall.  If the width is
+      // 800 and height 0 the image will be 800 pixels wide if the source
+      // is at least that wide.
+      width: 1000,
+      //height: 200,
+
+      // quality of resized image, defaults to 100
+      
+
+      // output type, defaults to FILE_URIs.
+      // available options are 
+      // window.imagePicker.OutputType.FILE_URI (0) or 
+      // window.imagePicker.OutputType.BASE64_STRING (1)
+      outputType: 1
+    };
+    
+    this.imagePicker.getPictures(this.options).then((results) => {
+      
+        this.imageResponse = ('data:image/jpeg;base64,' + results[0]);
+      
+    }, (err) => { 
+      alert(err);
+    });
+  }
+  
+
 
 }
