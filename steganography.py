eb70e097250fcee55f1ffa2f12963952ae7568b8
Iván Celis Maldonado <ivan.celis.ma@gmail.com>
2021-04-28 19:00:04 -0500

    b4 python3

--- steganography.py
+++ steganography.py
@@ -1,15 +1,15 @@
 from PIL import Image
 import binascii
-import optparse
+import argparse
 
 def rgb2hex(r,g,b):
     return '#{:02x}{:02x}{:02x}'.format(r,g,b)
 
 def hex2rgb(hexcode):
-    return tuple(map(ord, hexcode[1:].decode('hex')))
+    return tuple(map(ord, hexcode[1:]('hex')))
 
 def str2bin(message):
-    binary = bin(int(binascii.hexlify(message),16))
+    binary = bin(int(binascii.hexlify(message.encode()),16))
     # not storing the 0b
     return binary[2:]
 
@@ -55,5 +55,48 @@ def hideInfo(filename, message):
         img.save(filename, "PNG")
         return "Completed!"
     return "Incorret image mode, couldn't encode"
+
+def decodeInfo (filename):
+    img = Image.open(filename)
+    binary =''
+    if img.mode in ('RGBA'):
+        img = img.convert('RGBA')
+        datas = img.getdata()
+
+        for item in datas:
+            digit = decode(rgb2hex(item[0], item[1], item[2]))
+            if digit == None:
+                pass
+            else:
+                binary = binary + digit
+                #checking for delimiter.
+                if(binary[-16] == '1111111111111110'):
+                    print ("Success")
+                    return bin2str(binary[:-16])
+        return bin2str(binary)
+    return "Incorrect image mode, could not decode"
+
+
+def Main():
+    parser = argparse.ArgumentParser(usage ="usage %prog " + '-e/-d <image file>')
+    #just to prove it in console
+    parser.add_argument('-e', dest='encode', help = 'image file to hide text')
+    parser.add_argument('-d', dest='decode', help = 'image file to decode text from')
+
+    (args) = parser.parse_args()
+
+    if(args.encode != None):
+        text = input("Enter a message to hide: ")
+        #print(str2bin("hola"))
+        print (hideInfo(args.encode, text))
+    elif (args.decode != None):
+        print(decodeInfo(args.decode))
+    else:
+        print(parser.usage)
+        exit(0)
+
+if __name__ == '__main__':
+    Main()
     
+
 
